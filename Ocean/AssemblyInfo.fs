dacfa7394194caf82688b683a31e6c39c8e145c1
Rupert Muchembled <rupert@rmuch.net>
2014-05-28 19:31:38 +0100

    Add AssemblyInfo

---
+++ Ocean/AssemblyInfo.fs
@@ -0,0 +1,16 @@
+﻿module private Ocean.AssemblyInfo
+
+open System.Reflection
+
+[<assembly: AssemblyTitle("Ocean")>]
+[<assembly: AssemblyDescription("Ocean Web Framework Core Library")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Ocean Web Framework")>]
+[<assembly: AssemblyCopyright("Copyright © Rupert Muchembled 2014")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: AssemblyVersion("0.1.*")>]
+[<assembly: AssemblyFileVersion("0.1.*")>]
+do ()
