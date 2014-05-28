b25fae1f4bcf370df1f00f6d8b7ebcb3270936e3
Rupert Muchembled <rupert@rmuch.net>
2014-05-28 19:47:51 +0100

    Showcase: Add Ocean.Showcase project

---
+++ Ocean.Showcase/Program.fs
@@ -0,0 +1,13 @@
+﻿open Ocean
+open Ocean.Core
+
+let mainHandler req =
+    RespondWith.str """<h1>Ocean Showcase</h1>"""
+
+let routes =
+    [ Match.path "/", mainHandler ]
+
+[<EntryPoint>]
+let main argv =
+    FrameworkWebServer.serve "http://*:8080/" routes
+    0
